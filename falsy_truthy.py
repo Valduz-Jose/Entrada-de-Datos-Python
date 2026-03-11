7c0576539bec7e7780e204f73b770f85e5ba771a
Jose Valduz <jose.valduz@unet.edu.ve>
2026-03-11 13:37:24 -0400

    Agregar archivo que demuestra el uso de la función bool() en Python

---
+++ falsy_truthy.py
@@ -0,0 +1,16 @@
+# Funcion bool()
+
+print(bool(0))
+print(bool(0.0))
+print(bool(1))
+
+
+print(bool(""))
+print(bool("Hola"))
+
+vacio = None
+print(bool(vacio))
+
+print(bool(False))
+print(bool(True))
+
