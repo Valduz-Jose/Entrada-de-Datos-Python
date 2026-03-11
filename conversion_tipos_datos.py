ba422f95b516eee5698b55c0ebc7352d13a5f84d
Jose Valduz <jose.valduz@unet.edu.ve>
2026-03-11 13:32:33 -0400

    Agregar archivo de conversión de tipos de datos en Python

---
+++ conversion_tipos_datos.py
@@ -0,0 +1,11 @@
+# Casting -> Es el proceso de convertir un tipo de dato a otro tipo de dato.
+
+# int()
+# float()
+# str()
+
+numero_texto = "50"
+numero_entero = int(numero_texto)
+concatenacion = numero_texto + str(69)
+print(numero_entero+69)
+print(concatenacion)
