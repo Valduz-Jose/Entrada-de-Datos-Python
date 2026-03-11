1508f3454358e8ecafa687c0df46b196633fc246
Jose Valduz <jose.valduz@unet.edu.ve>
2026-03-11 13:45:06 -0400

    Agregar archivo de entrada de datos en Python

---
+++ entrada_de_datos.py
@@ -0,0 +1,15 @@
+# Permite el ingreso de datpos al programa
+# variable = input("Escribe tu nombre:")
+# Siempre devuelve un string 
+
+nombre = input("Escribe tu Nombre: ")
+print(f"Hola {nombre}, bienvenido al curso de Python")
+
+# cuidado con la conversion de tipos
+edad = int(input("Escribe tu edad: "))
+print(f"Tu edad es: {edad} años")
+print(edad+5)
+
+# decimales
+altura = float(input("Escribe tu altura: "))
+print(f"Tu altura es: {altura} metros")
